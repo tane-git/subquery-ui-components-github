ef4429598d01439e78d06729da94584ea3ad1993
tane-git <tane.alexander.wilson@gmail.com>
2022-02-28 16:48:47 +1300

    copy and paste bitbucket repo

---
+++ types/css.d.ts
@@ -0,0 +1,9 @@
+// Copyright 2020-2021 OnFinality Limited authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+declare module '*.module.css' {
+  const classes: {
+    [key: string]: string;
+  };
+  export default classes;
+}
